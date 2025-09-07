065c756e5e41d0343c0072a13e40810154ba7ea5
mikk39k1 <mikkel@goodtape.io>
2025-09-07 19:51:26 +0200

    Color changes fonts image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const inter = Inter({
+  variable: "--font-inter",
   subsets: ["latin"],
 });
 
@@ -11,6 +11,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,14 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="h-screen w-full flex flex-col items-center justify-center bg-gray-100">
-      <h1 className="text-4xl font-bold text-center">Lise & Mikkel</h1>
-      <p className="text-center">Mere info kommer snart</p>
+    <div className="h-screen w-full flex flex-col items-center justify-center bg-[#fffefa]">
+      <div className="flex flex-col items-center justify-center mb-8">
+      <h1 className="text-4xl font-light text-center">LISE & MIKKEL</h1>
+      {/* <div className="w-[110%] border-b border-gray-200 pb-1" /> */}
+      <h2 className="text-2xl text-center">Bryllupsfest</h2>
+      </div>
+      <p className="text-center text-small mb-8">Mere info kommer snart</p>
+      <Image src="/images/bryllupsblomst.jpg" width={300} height={300} alt="Bryllupsblomst" />
     </div>
   );
 }
