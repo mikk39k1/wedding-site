dd631c4eb266716db98c6bd370f65a145a3f7de6
mikk39k1 <mikkel@goodtape.io>
2025-09-26 11:33:46 +0200

    Added info date

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <div className="absolute bottom-1/5 right-4 flex flex-col items-end justify-center">
         <h1 className="text-4xl font-light text-right text-white">LISE & MIKKEL</h1>
         <h2 className="text-2xl text-right text-white mr-2">Bryllupsfest</h2>
-        <p className="text-right text-small mb-8 text-white mr-2">Mere info kommer snart</p>
+        <p className="text-right text-small mb-8 text-white mr-2">Mere info kommer 01.12.2025</p>
       </div>
 
       {/* <Image src="/images/bryllupsblomst.png" width={300} height={300} alt="Bryllupsblomst" /> */}
